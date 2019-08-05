9e6433322a27715a6eac0fb5a46835ad632afdc9
Maruthi S Inukonda <maruthi.inukonda@gmail.com>
2019-08-06 05:04:35 +0530

    manual drive node (courtesy https://github.com/dkarunakaran/robocar) and its launch file xml snippet

--- README.md
+++ README.md
@@ -28,11 +28,41 @@ Topics Published:
 Initial:
 
 ```
-cs18resch01001@nano-ub18:~/AllCode/Maruthi/chinkara/donkeycar_1906$ rosnode list
+cs18resch01001@nano-ub18:~$ rosnode list
 /rosout
-cs18resch01001@nano-ub18:~/AllCode/Maruthi/chinkara/donkeycar_1906$ rostopic list
+cs18resch01001@nano-ub18:~$ rostopic list
 /rosout
 /rosout_agg
+```
+
+By Logitech F710 joystick:
+
+```
+cs18resch01001@nano-ub18:~$ rosparam set joy_node/dev "/dev/input/js0"
+
+cs18resch01001@nano-ub18:~$ rosrun joy joy_node
+[ERROR] [1564965364.681140559]: Couldn't open joystick force feedback!
+[ INFO] [1564965364.683083845]: Opened joystick: /dev/input/js0. deadzone_: 0.050000.
+
+cs18resch01001@nano-ub18:~$ rostopic list
+/diagnostics
+/joy
+/joy/set_feedback
+/rosout
+/rosout_agg
+
+cs18resch01001@nano-ub18:~$ rosnode list
+/joy_node
+/rosout
+
+cs18resch01001@nano-ub18:~$ rostopic hz /joy
+subscribed to [/joy]
+no new messages
+no new messages
+no new messages
+no new messages
+^Cno new messages
+
 ```
 
 By CSI camera:
---
+++ src/donkeycar/src/ros_nodes/manual_drive_node.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+#
+# Copyright (C) 2018-2019 Maruthi Seshidhar Inukonda - All Rights Reserved.
+# maruthi.inukonda@gmail.com
+#
+# This file is released under the GPLv3 License.
+#
+
+import rospy
+from sensor_msgs.msg import Joy
+from servo_motor_controller import ServoMotorController
+
+class DonkeyCarManualDriver:
+    const_throttle = rospy.get_param("/CONST_THROTTLE")
+    #Steering constants
+    max_left_angle = rospy.get_param("/MAX_LEFT_ANGLE")
+    min_right_angle = rospy.get_param("/MIN_RIGHT_ANGLE")
+    max_steering_pulse = rospy.get_param("/MAX_STEERING_PULSE")
+    min_steering_pulse = rospy.get_param("/MIN_STEERING_PULSE")
+    start_recording  = rospy.get_param("/START_RECORDING")
+    steering_channel = rospy.get_param("/STEERING_CHANNEL")
+
+    #Throttle constants
+    min_throttle = rospy.get_param("/MIN_THROTTLE")
+    max_throttle = rospy.get_param("/MAX_THROTTLE")
+    zero_pulse = rospy.get_param("/ZERO_PULSE")
+    min_throttle_pulse = rospy.get_param("/MIN_THROTTLE_PULSE")
+    max_throttle_pulse = rospy.get_param("/MAX_THROTTLE_PULSE")
+    throttle_channel = rospy.get_param("/THROTTLE_CHANNEL")
+    max_reverse_throttle = rospy.get_param("/MAX_REVERSE_THROTTLE")
+    const_throttle_step = rospy.get_param("/CONST_THROTTLE_STEP")
+
+    def __init__(self):
+        # Initialize the Servo Motor Controller
+        self.const_throttle = rospy.get_param("/CONST_THROTTLE")
+        self.smc = ServoMotorController()
+
+    def getActuatorPulseValue(self, joystickValue, joystickMin, joystickMax, actuatorMin, actuatorMax, channel_type='steering'):
+        #Linear mapping between two ranges of values
+        joystickRange = joystickMax - joystickMin
+        actuatorRange = actuatorMax - actuatorMin
+
+        joystickActuatorRatio = float(joystickRange)/float(actuatorRange)
+        actuatorValue = int((joystickValue - joystickMin) / joystickActuatorRatio + actuatorMin)	   
+        if channel_type == 'steering':
+            actuatorValue +=18
+
+        return actuatorValue
+
+    def find_max_throttle(self, throttle_pulse):
+        throttle = self.const_throttle
+        if throttle_pulse <= self.const_throttle:
+            throttle = throttle_pulse
+        if throttle_pulse < self.max_reverse_throttle:
+            throttle = self.max_reverse_throttle
+
+        return throttle
+
+    def joyStickSubscriberCallback(self, msg):
+        rospy.loginfo("Received len(axes):%d", len(msg.axes)) # 8
+        rospy.loginfo("Received len(buttons):%d", len(msg.buttons)) #11
+        '''
+        Received:['__class__', '__delattr__', '__doc__', '__eq__', '__format__', '__getattribute__', '__getstate__', '__hash__', '__init__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__setstate__', '__sizeof__', '__slots__', '__str__', '__subclasshook__', '_check_types', '_connection_header', '_full_text', '_get_types', '_has_header', '_md5sum', '_slot_types', '_type', 'axes', 'buttons', 'deserialize', 'deserialize_numpy', 'header', 'serialize', 'serialize_numpy']
+        '''
+        steering_pulse = self.getActuatorPulseValue(msg.axes[0], self.min_right_angle, self.max_left_angle, self.min_steering_pulse, self.max_steering_pulse, channel_type='steering')
+        throttle_pulse = self.find_max_throttle(msg.axes[3])
+        throttle_pulse = self.getActuatorPulseValue(throttle_pulse, self.min_throttle, self.max_throttle, self.min_throttle_pulse, self.max_throttle_pulse, channel_type='throttle')
+        self.smc.set_channel_pulse(self.steering_channel, pulse=steering_pulse)
+        self.smc.set_channel_pulse(self.throttle_channel, pulse=throttle_pulse)
+        rospy.loginfo('Steering value: %s', steering_pulse)
+        rospy.loginfo('Throttle value: %s', throttle_pulse)
+        #rospy.loginfo('Maximum throttle value: %s', self.const_throttle)
+        #rospy.loginfo('Joystic throttle value: %s', data.axes[3])
+
+if __name__ == '__main__':
+    driver = DonkeyCarManualDriver()
+    rospy.init_node('manual_drive_node')
+    rospy.loginfo("[ROS manual_drive_node] started.")
+
+    rospy.Subscriber('/joy', Joy, driver.joyStickSubscriberCallback, queue_size=100)
+
+    rospy.spin()
+
